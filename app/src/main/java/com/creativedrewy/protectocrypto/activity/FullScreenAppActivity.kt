37ef8d42e460ad663943c168c0f5d9fd864ad061
Andrew <creativedrewy@gmail.com>
2020-10-19 17:10:43 -0700

    Make activity a text share destination, get sheet revealing logic to intially work.

--- app/src/main/java/com/creativedrewy/protectocrypto/activity/FullScreenAppActivity.kt
+++ app/src/main/java/com/creativedrewy/protectocrypto/activity/FullScreenAppActivity.kt
@@ -1,26 +1,47 @@
 package com.creativedrewy.protectocrypto.activity
 
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.creativedrewy.protectocrypto.R
 import com.creativedrewy.protectocrypto.fragment.EncryptDecryptFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class FullScreenAppActivity : AppCompatActivity() {
 
+    private var sheetFragment: EncryptDecryptFragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.full_screen_activity)
 
-        //if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                    .replace(R.id.container, EncryptDecryptFragment.newInstance())
-//                    .commitNow()
+        createAndRevealSheet()
+    }
 
-            val fragment = EncryptDecryptFragment.newInstance()
-            fragment.show(supportFragmentManager, "ui")
-        //}
+    override fun onStart() {
+        super.onStart()
 
+        if (sheetFragment == null) {
+            createAndRevealSheet()
+        } else {
+            sheetFragment?.let {
+                val sheetRef = it.view?.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+                sheetRef?.let {
+                    BottomSheetBehavior.from(sheetRef).state = BottomSheetBehavior.STATE_EXPANDED
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        sheetFragment = null
+    }
+
+    private fun createAndRevealSheet() {
+        sheetFragment = EncryptDecryptFragment.newInstance()
+        sheetFragment?.show(supportFragmentManager, "ui")
     }
 }
