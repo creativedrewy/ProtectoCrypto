975d39afb89abc917ea383a50df8990ef534ee3d
Andrew <creativedrewy@gmail.com>
2020-10-23 13:03:46 -0700

    Convert input fields to be TextInputLayouts

--- app/src/main/java/com/creativedrewy/protectocrypto/fragment/EncryptDecryptFragment.kt
+++ app/src/main/java/com/creativedrewy/protectocrypto/fragment/EncryptDecryptFragment.kt
@@ -53,9 +53,9 @@ class EncryptDecryptFragment : Fragment() {
         viewModel.viewState.observe(this, Observer { state ->
             when (state) {
                 is DataProcessed -> {
-                    key_input_textview.setText(state.sourceKey)
-                    data_input_textview.setText(state.sourceData)
-                    operation_result_textview.setText(state.processingResult)
+                    key_input_textview.editText?.setText(state.sourceKey)
+                    data_input_textview.editText?.setText(state.sourceData)
+                    operation_result_textview.editText?.setText(state.processingResult)
                 }
                 is ErrorState -> {
                     Snackbar.make(main, state.message, Snackbar.LENGTH_SHORT).show()
@@ -65,13 +65,13 @@ class EncryptDecryptFragment : Fragment() {
 
         encrypt_button.setOnClickListener {
             validateForm {
-                viewModel.encodeData(key_input_textview.text.toString(), data_input_textview.text.toString())
+                viewModel.encodeData(key_input_textview.editText?.text.toString(), data_input_textview.editText?.text.toString())
             }
         }
 
         decrypt_button.setOnClickListener {
             validateForm {
-                viewModel.decodeData(key_input_textview.text.toString(), data_input_textview.text.toString())
+                viewModel.decodeData(key_input_textview.editText?.text.toString(), data_input_textview.editText?.text.toString())
             }
         }
 
@@ -95,7 +95,8 @@ class EncryptDecryptFragment : Fragment() {
     }
 
     private fun validateForm(onValidated: () -> Unit) {
-        if (key_input_textview.text.isNotEmpty() && data_input_textview.text.isNotEmpty()) {
+        if (key_input_textview.editText?.text?.isNotEmpty() == true &&
+            data_input_textview.editText?.text?.isNotEmpty() == true) {
             onValidated()
         }
     }
